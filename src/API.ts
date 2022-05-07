ffc36094e49f26a3c28ac661d7d4fcde323c9034
ChoiJunSu <keroro10398@gmail.com>
2022-05-07 22:10:06 +0900

    Amplify codegen

--- src/API.ts
+++ src/API.ts
@@ -3,144 +3,152 @@
 //  This file was automatically generated and should not be edited.
 
 export type Place = {
-  __typename: "Place";
-  id: number;
-  name: string;
-  description?: string | null;
-  latitude: number;
-  longitude: number;
-  parking?: number | null;
-  toilet?: number | null;
-  createdAt: number;
-  updatedAt: number;
+  __typename: "Place",
+  id: number,
+  name: string,
+  description?: string | null,
+  latitude: number,
+  longitude: number,
+  parking?: number | null,
+  toilet?: boolean | null,
+  createdAt: string,
+  updatedAt: string,
 };
 
 export type CreatePlaceInput = {
-  id: number;
-  name: string;
-  description?: string | null;
-  latitude: number;
-  longitude: number;
-  parking?: number | null;
-  toilet?: number | null;
-  createdAt: number;
-  updatedAt: number;
+  id?: number | null,
+  name: string,
+  description?: string | null,
+  latitude: number,
+  longitude: number,
+  parking?: number | null,
+  toilet?: boolean | null,
+  createdAt?: string | null,
+  updatedAt?: string | null,
 };
 
 export type UpdatePlaceInput = {
-  id: number;
-  name?: string | null;
-  description?: string | null;
-  latitude?: number | null;
-  longitude?: number | null;
-  parking?: number | null;
-  toilet?: number | null;
-  createdAt?: number | null;
-  updatedAt?: number | null;
+  id: number,
+  name?: string | null,
+  description?: string | null,
+  latitude?: number | null,
+  longitude?: number | null,
+  parking?: number | null,
+  toilet?: boolean | null,
+  createdAt?: string | null,
+  updatedAt?: string | null,
 };
 
 export type DeletePlaceMutationVariables = {
-  id: number;
+  id: number,
 };
 
 export type DeletePlaceMutation = {
-  deletePlace?: {
-    __typename: "Place";
-    id: number;
-    name: string;
-    description?: string | null;
-    latitude: number;
-    longitude: number;
-    parking?: number | null;
-    toilet?: number | null;
-    createdAt: number;
-    updatedAt: number;
-  } | null;
+  deletePlace?:  {
+    __typename: "Place",
+    id: number,
+    name: string,
+    description?: string | null,
+    latitude: number,
+    longitude: number,
+    parking?: number | null,
+    toilet?: boolean | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
 };
 
 export type CreatePlaceMutationVariables = {
-  createPlaceInput: CreatePlaceInput;
+  createPlaceInput: CreatePlaceInput,
 };
 
 export type CreatePlaceMutation = {
-  createPlace?: {
-    __typename: "Place";
-    id: number;
-    name: string;
-    description?: string | null;
-    latitude: number;
-    longitude: number;
-    parking?: number | null;
-    toilet?: number | null;
-    createdAt: number;
-    updatedAt: number;
-  } | null;
+  createPlace?:  {
+    __typename: "Place",
+    id: number,
+    name: string,
+    description?: string | null,
+    latitude: number,
+    longitude: number,
+    parking?: number | null,
+    toilet?: boolean | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
 };
 
 export type UpdatePlaceMutationVariables = {
-  updatePlaceInput: UpdatePlaceInput;
+  updatePlaceInput: UpdatePlaceInput,
 };
 
 export type UpdatePlaceMutation = {
-  updatePlace?: {
-    __typename: "Place";
-    id: number;
-    name: string;
-    description?: string | null;
-    latitude: number;
-    longitude: number;
-    parking?: number | null;
-    toilet?: number | null;
-    createdAt: number;
-    updatedAt: number;
-  } | null;
+  updatePlace?:  {
+    __typename: "Place",
+    id: number,
+    name: string,
+    description?: string | null,
+    latitude: number,
+    longitude: number,
+    parking?: number | null,
+    toilet?: boolean | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
 };
 
 export type GetPlaceQueryVariables = {
-  id: number;
+  id: number,
 };
 
 export type GetPlaceQuery = {
-  getPlace?: {
-    __typename: "Place";
-    id: number;
-    name: string;
-    description?: string | null;
-    latitude: number;
-    longitude: number;
-    parking?: number | null;
-    toilet?: number | null;
-    createdAt: number;
-    updatedAt: number;
-  } | null;
+  getPlace?:  {
+    __typename: "Place",
+    id: number,
+    name: string,
+    description?: string | null,
+    latitude: number,
+    longitude: number,
+    parking?: number | null,
+    toilet?: boolean | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
+};
+
+export type ListPlacesQueryVariables = {
+  name?: string | null,
+  neLat?: number | null,
+  neLng?: number | null,
+  swLat?: number | null,
+  swLng?: number | null,
 };
 
 export type ListPlacesQuery = {
-  listPlaces?: Array<{
-    __typename: "Place";
-    id: number;
-    name: string;
-    description?: string | null;
-    latitude: number;
-    longitude: number;
-    parking?: number | null;
-    toilet?: number | null;
-    createdAt: number;
-    updatedAt: number;
-  } | null> | null;
+  listPlaces?:  Array< {
+    __typename: "Place",
+    id: number,
+    name: string,
+    description?: string | null,
+    latitude: number,
+    longitude: number,
+    parking?: number | null,
+    toilet?: boolean | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null > | null,
 };
 
 export type OnCreatePlaceSubscription = {
-  onCreatePlace?: {
-    __typename: "Place";
-    id: number;
-    name: string;
-    description?: string | null;
-    latitude: number;
-    longitude: number;
-    parking?: number | null;
-    toilet?: number | null;
-    createdAt: number;
-    updatedAt: number;
-  } | null;
+  onCreatePlace?:  {
+    __typename: "Place",
+    id: number,
+    name: string,
+    description?: string | null,
+    latitude: number,
+    longitude: number,
+    parking?: number | null,
+    toilet?: boolean | null,
+    createdAt: string,
+    updatedAt: string,
+  } | null,
 };

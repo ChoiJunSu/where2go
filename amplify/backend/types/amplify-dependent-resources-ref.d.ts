928cb3515185378b7cee9e6662747963c4d565b5
ChoiJunSu <keroro10398@gmail.com>
2022-05-07 21:56:27 +0900

    Re-init amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "where2go": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
